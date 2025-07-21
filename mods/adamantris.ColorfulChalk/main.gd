736d0c15c5e9468e82fce7d60f66e1520d39abde
adamantris <noreply>
2025-07-21 21:11:41 +0200

    tried initial networking

--- mods/adamantris.ColorfulChalk/main.gd
+++ mods/adamantris.ColorfulChalk/main.gd
@@ -2,6 +2,14 @@ extends Node
 
 signal chalk_color_changed
 
+enum send_type{
+	RELIABLE = 8,
+	UNRELIABLE = 0,
+	UNRELIABLE_FAST = 4,
+}
+
+const COLOR_CHANNEL = 10
+
 onready var Lure = get_node("/root/SulayreLure")
 onready var Players = get_node_or_null("/root/ToesSocks/Players")
 onready var Chat = get_node_or_null("/root/ToesSocks/Chat")
@@ -14,6 +22,8 @@ var selected_chalk_color
 var Lure_chalk_dict
 var Lure_chalk_resource
 
+var packet_timer: float = 0.0
+
 onready var color_string #: String = "#ff0000ff"
 onready var color
 export onready var img_data
@@ -23,13 +33,25 @@ export onready var img_data
 func _ready():
 	Chat.connect("player_messaged", self, "set_color")
 	Players.connect("ingame", self, "ingame")
-	Lure.add_content("adamantris.ColorfulChalk", "Rainbow Chalk", "res://mods/adamantris.ColorfulChalk/chalk_special.tres", [Lure.LURE_FLAGS.FREE_UNLOCK])
+	Lure.add_content("adamantris.ColorfulChalk", "Rainbow Chalk", "res://mods/adamantris.ColorfulChalk/chalk_rainbow.tres", [Lure.LURE_FLAGS.FREE_UNLOCK])
+	#Steam.connect("lobby_joined", self, "lobby_joined")
+	
+	
+	
 	#string_to_color(color_string)
 	pass # Replace with function body.
+	
+func _process(delta):
+	if packet_timer >= 0.1:
+		read_packets()
+		
+	else:
+		packet_timer += delta
 
 func ingame(): #to get the tilemap, gotta add dynamically after all or the whole world explodes
 	Lure_chalk_dict = Lure.item_list.get("adamantris.ColorfulChalk.Rainbow Chalk")
 	Lure_chalk_resource = Lure_chalk_dict.get("resource")
+	lobby_joined()
 	
 	
 	print(str(Lure_chalk_resource))
@@ -83,6 +105,8 @@ func create_new_tile(color: Color): #in hex value
 	var tileset_id = canvas_TileSet.get_last_unused_tile_id()
 	color_dict[color_string] = tileset_id
 	
+	print(str(color_dict))
+	
 	#lets add the textured tile already
 	var img_texture = ImageTexture.new()
 	img_texture.create_from_image(img_data)
@@ -99,3 +123,38 @@ func create_new_tile(color: Color): #in hex value
 	print("AAAAA hopefully a new tile has been created: " + str(canvas_TileSet.tile_get_name(tileset_id)) + " " + str(canvas_TileSet.tile_get_texture(tileset_id)))
 	
 
+func lobby_joined():
+	var color_handshake = "hello_lobby_i_just_joined"
+	
+	var hello_poolbyte = var2bytes(["color_handshake", color_handshake])
+	print("attempting to send a hello")
+	for connection_entry in Network.OPEN_CONNECTIONS:
+		var steam_id = connection_entry
+		Steam.sendMessageToUser(str(steam_id), hello_poolbyte, send_type.RELIABLE, COLOR_CHANNEL)
+		print("heyyy i sent a hello to " + str(steam_id))
+		
+		
+func read_packets():
+	
+	var message_array = Steam.receiveMessagesOnChannel(COLOR_CHANNEL, 5)
+	if message_array.empty():
+		return
+		
+	else:
+		for message in message_array:
+			var decoded = bytes2var(message.get("payload"))
+			var packet_command = decoded[0]
+			
+			match packet_command:
+				
+				"color_handshake":
+					var respond_handshake = "hi yes i see you"
+					var response_poolbyte = var2bytes(["response", respond_handshake])
+					Steam.sendMessageToUser(str(decoded[3]), response_poolbyte, send_type.RELIABLE, COLOR_CHANNEL)
+					print("i sent a response to the handshake")
+					
+				"response":
+					print("ooo " + Players.get_usernme(decoded[3]) + " sent a response")
+					
+		
+	
