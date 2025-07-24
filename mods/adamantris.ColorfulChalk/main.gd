3bf1a65f161819a7f7fbeb774a98fb9e5455550d
adamantris <noreply>
2025-07-24 20:51:49 +0200

    added mod user tracking, tile and player cleanup on disconnect and moved networking around a bit

--- mods/adamantris.ColorfulChalk/main.gd
+++ mods/adamantris.ColorfulChalk/main.gd
@@ -22,6 +22,8 @@ var selected_chalk_color
 var Lure_chalk_dict
 var Lure_chalk_resource
 
+var mod_user_list = []
+
 var packet_timer: float = 0.0
 
 var global_color_string: String = "#ff0000ff"
@@ -33,7 +35,10 @@ func _ready():
 	yield(get_tree().create_timer(1.0), "timeout")
 	Chat.connect("player_messaged", self, "set_color")
 	Players.connect("ingame", self, "ingame")
+	Players.connect("player_removed", self, "player_removed")
 	Lure.add_content("adamantris.ColorfulChalk", "Rainbow Chalk", "res://mods/adamantris.ColorfulChalk/chalk_rainbow.tres", [Lure.LURE_FLAGS.FREE_UNLOCK])
+	
+	
 	#Steam.connect("lobby_joined", self, "lobby_joined")
 	
 	
@@ -155,9 +160,38 @@ func lobby_joined():
 		print("heyyy i sent a hello to " + str(steam_id))
 		
 		
+func request_dict():
+	if mod_user_list.size() >= 1:
+		yield(get_tree().create_timer(0.5), "timeout") #waiting a lil bit for the dict to populate
+		var request_poolbyte = var2bytes(["requested_dict", "pls send dict i want to see colors"])
+		
+		Steam.sendMessageToUser(mod_user_list[0], request_poolbyte, send_type.RELIABLE, COLOR_CHANNEL) #asking the first available user for already created colors
+	
+	else:
+		pass
+		
+
+func player_removed(player_node):
+	
+	if player_node.owner_id in mod_user_list:
+		mod_user_list.erase(player_node.owner_id)
+		print("a player left, removing id: " + str(player_node.owner_id))
+		
+	elif player_node.owner_id == Network.STEAM_ID: #it took me too long to implement dict clearing lol im lazy
+		print("you left, emptying player list, removing created tiles and color dict")
+		
+		for tile in color_dict.keys():
+			canvas_TileSet.remove_tile(color_dict.get(tile))
+		
+		print("tried to remove tiles, tiles left: " + str(canvas_TileSet.get_tiles_ids()))
+		
+		color_dict = {}
+		mod_user_list.empty()
+
+
 func read_packets():
-	var valid_commands = ["color_handshake", "handshake_response", "create_new_color"] #this is stupid but maybe it works
-	var message_array = Steam.receiveMessagesOnChannel(COLOR_CHANNEL, 5)
+	var valid_commands = ["color_handshake", "handshake_response", "create_new_color", "requested_dict", "received_dict"] #this is stupid but maybe it works
+	var message_array = Steam.receiveMessagesOnChannel(COLOR_CHANNEL, 10)
 	#print(str(message_array.size()))
 	
 	#print("rolling over packets now")
@@ -172,29 +206,58 @@ func read_packets():
 			match packet_command:
 				
 				"color_handshake":
-					var response_poolbyte = var2bytes(["handshake_response", color_dict.keys()])
-					
+					var response_poolbyte = var2bytes(["handshake_response", "hello_back"])
+					#adding responding players to an array, i dont want to round-robin packets to all players constantly
+					print("received a handshake, responding and adding to player list")
+					mod_user_list.append(sender_steam_id)
+
 					Steam.sendMessageToUser(sender_steam_id, response_poolbyte, send_type.RELIABLE, COLOR_CHANNEL)
-					print("received a handshake, sending the color dict")
+					
 					
 				"handshake_response":
-					print("received a response, creating colors...")
+					print("received a response, stuffing player " + sender_steam_id + " into mod user list")
+					mod_user_list.append(sender_steam_id)
+					
+					print("asking first available player for the color dict")
+					request_dict()
+					
+				"requested_dict":
+					print("received a request for the color dict, sending it since theyre asking so nicely")
+					
+					var dict_poolbyte = var2bytes(["sent_dict", color_dict])
+					Steam.sendMessageToUser(sender_steam_id, dict_poolbyte, send_type.RELIABLE, COLOR_CHANNEL)
+					
+					
+				"sent_dict":
+					print("received a color dict, turning into colors")
+					
+					for color_string in decoded[1].keys():
+						
+						if typeof(color_string) == TYPE_STRING and color_string.length() == 9:
+							
+							string_to_color(color_string)
+							
+						else:
+							print("hey something went wrong with the received dict, this is the type of one entry: " + str(typeof(color_string)))
+							PlayerData._send_notification("something went wrong in recreating the old colors of a player, yell at ferrum and show them this: " + str(typeof(color_string)) + " " + str(color_string))
+							
+					
 					print(str(decoded[1]) + str(typeof(decoded[1])))
 					
-					if color_dict.empty():
-						for color_entry in decoded[1]:
-							print("section out of received packet from handshake: " + str(color_entry) + " " + str(typeof(color_entry)))
-							if typeof(color_entry) != TYPE_STRING:
-								print("invalid variant type, want strin, but getting " + str(color_entry))
-								PlayerData._send_notification("yell this at ferrum pls: " + str(color_entry))
-							
-							else:
-								print("should have received a string " + color_entry)
-								var dict_color = Color(color_entry)
-								create_new_tile(dict_color)
-							
-					else:
-						pass
+#					if color_dict.empty():
+#						for color_entry in decoded[1]:
+#							print("section out of received packet from handshake: " + str(color_entry) + " " + str(typeof(color_entry)))
+#							if typeof(color_entry) != TYPE_STRING:
+#								print("invalid variant type, want strin, but getting " + str(color_entry))
+#								PlayerData._send_notification("yell this at ferrum pls: " + str(color_entry))
+#
+#							else:
+#								print("should have received a string " + color_entry)
+#								var dict_color = Color(color_entry)
+#								create_new_tile(dict_color)
+#
+#					else:
+#						pass
 						
 				"create_new_color":
 					print("someone created a new color, passing on to create_new_tile")
