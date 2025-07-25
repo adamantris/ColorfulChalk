05228908b4097e69593ae9ccda5366eb469f0c59
adamantris <noreply>
2025-07-26 00:46:21 +0200

    connected the stupid color picker, its so jank

---
+++ mods/adamantris.ColorfulChalk/scenes/picker.gd
@@ -0,0 +1,22 @@
+extends ColorPicker
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+onready var save_button = $"../../Button"
+onready var main = $"/root/adamantrisColorfulChalk"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print("did i find a button? " + str(save_button))
+	save_button.connect("pressed", self, "button_pressed")
+	pass # Replace with function body.
+
+
+func button_pressed():
+	var color_string = "#" + self.color.to_html()
+	print("ayooooo we got a color signal " + color_string)
+	main.global_color_string = color_string
+	main.string_to_color(color_string)
